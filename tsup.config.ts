95a16bb7f923f9d9c639aee3869254bfc4df3325
Edie Lemoine <edie@myparcel.nl>
2023-12-11 10:50:08 +0100

    fix: update final package bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
-import {Options} from 'tsup';
+import {type Options} from 'tsup';
 
 const config: Options = {
   dts: true,
   entry: ['./src/index.ts', './src/countries/index.ts', './src/states/index.ts', './src/helpers/index.ts'],
   format: ['cjs', 'esm'],
-  minify: false,
+  minify: true,
   target: 'node16',
   tsconfig: 'tsconfig.build.json',
 };
