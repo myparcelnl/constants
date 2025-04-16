6f52cda2a329049cebf13fc52065b7ed3535ecce
fleurvaneijk <37218048+fleurvaneijk@users.noreply.github.com>
2025-04-16 14:23:11 +0200

    feat: CI-111 - add GLS, UPS Standard & UPS Express Saver (#127)
    voeg GLS, UPS Standard & UPS Express Saver toe

--- src/types/carrier.ts
+++ src/types/carrier.ts
@@ -10,10 +10,14 @@ export enum CarrierId {
   Bol = 7,
   // @deprecated Use CarrierId.Bol instead. Will be removed in the next major version
   BolCom = 7,
+  // @deprecated Use CarrierId.UpsStandard instead.
   Ups = 8,
   DhlForYou = 9,
   DhlParcelConnect = 10,
   DhlEuroPlus = 11,
+  UpsStandard = 12,
+  UpsExpressSaver = 13,
+  Gls = 14,
 }
 
 export enum CarrierName {
@@ -26,10 +30,14 @@ export enum CarrierName {
   Bol = 'bol.com',
   // @deprecated Use CarrierName.Bol instead. Will be removed in the next major version
   BolCom = 'bol.com',
+  // @deprecated Use CarrierName.UpsStandard instead.
   Ups = 'ups',
   DhlForYou = 'dhlforyou',
   DhlParcelConnect = 'dhlparcelconnect',
   DhlEuroPlus = 'dhleuroplus',
+  UpsStandard = 'upsstandard',
+  UpsExpressSaver = 'upsexpresssaver',
+  Gls = 'gls',
 }
 
 export type CarrierNameOrId = CarrierName | CarrierId;
@@ -50,6 +58,9 @@ export const CARRIER_NAMES_TO_IDS = {
   [CarrierName.DhlForYou]: CarrierId.DhlForYou,
   [CarrierName.DhlParcelConnect]: CarrierId.DhlParcelConnect,
   [CarrierName.DhlEuroPlus]: CarrierId.DhlEuroPlus,
+  [CarrierName.UpsStandard]: CarrierId.UpsStandard,
+  [CarrierName.UpsExpressSaver]: CarrierId.UpsExpressSaver,
+  [CarrierName.Gls]: CarrierId.Gls,
 } as const;
 
 export const CARRIER_IDS_TO_NAMES = {
@@ -64,4 +75,7 @@ export const CARRIER_IDS_TO_NAMES = {
   [CarrierId.DhlForYou]: CarrierName.DhlForYou,
   [CarrierId.DhlParcelConnect]: CarrierName.DhlParcelConnect,
   [CarrierId.DhlEuroPlus]: CarrierName.DhlEuroPlus,
+  [CarrierId.UpsStandard]: CarrierName.UpsStandard,
+  [CarrierId.UpsExpressSaver]: CarrierName.UpsExpressSaver,
+  [CarrierId.Gls]: CarrierName.Gls,
 } as const;
