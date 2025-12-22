3e65178844749b807f79cc4fc16626b6e8d27360
Freek van Rijt <freek.vanrijt@myparcel.nl>
2025-12-22 11:15:00 +0100

    feat: add platform italy (#132)

--- src/types/platform.ts
+++ src/types/platform.ts
@@ -4,18 +4,21 @@ export enum PlatformId {
   MyParcel = 1,
   Flespakket = 2,
   SendMyParcel = 3,
+  Italy = 6,
 }
 
 export enum PlatformName {
   MyParcel = 'myparcel',
   Flespakket = 'flespakket',
   SendMyParcel = 'belgie',
+  Italy = 'italy',
 }
 
 export enum PlatformDomain {
   MyParcel = 'myparcel.nl',
   Flespakket = 'flespakket.nl',
   SendMyParcel = 'sendmyparcel.be',
+  Italy = 'myparcel.com',
 }
 
 export type PlatformNameOrId = PlatformName | PlatformId;
@@ -28,22 +31,26 @@ export const PLATFORM_NAMES_TO_IDS = {
   [PlatformName.MyParcel]: PlatformId.MyParcel,
   [PlatformName.SendMyParcel]: PlatformId.SendMyParcel,
   [PlatformName.Flespakket]: PlatformId.Flespakket,
+  [PlatformName.Italy]: PlatformId.Italy,
 };
 
 export const PLATFORM_IDS_TO_NAMES = {
   [PlatformId.MyParcel]: PlatformName.MyParcel,
   [PlatformId.SendMyParcel]: PlatformName.SendMyParcel,
   [PlatformId.Flespakket]: PlatformName.Flespakket,
+  [PlatformId.Italy]: PlatformName.Italy,
 };
 
 export const PLATFORM_IDS_TO_DOMAINS = {
   [PlatformId.MyParcel]: PlatformDomain.MyParcel,
   [PlatformId.SendMyParcel]: PlatformDomain.SendMyParcel,
   [PlatformId.Flespakket]: PlatformDomain.Flespakket,
+  [PlatformId.Italy]: PlatformDomain.Italy,
 };
 
 export const PLATFORM_NAMES_TO_DOMAINS = {
   [PlatformName.MyParcel]: PlatformDomain.MyParcel,
   [PlatformName.SendMyParcel]: PlatformDomain.SendMyParcel,
   [PlatformName.Flespakket]: PlatformDomain.Flespakket,
+  [PlatformName.Italy]: PlatformDomain.Italy,
 };
